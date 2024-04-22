1220ceeaa174bbde54bb18fcab1d82560929935a
inhwa6854 <inhwa6854@gmail.com>
2024-04-22 18:06:56 +0900

    4주차입니당

---
+++ week4/javascript/ switch.js
@@ -0,0 +1,11 @@
+var n =2;
+switch(n){
+    case 1:
+        console.log("1번을 선택했습니다.");
+        break;
+    case 2:
+        console.log("2번을 선택했습니다.");
+        break;
+    default:
+        break;
+}
