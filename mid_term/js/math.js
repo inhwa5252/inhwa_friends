84d49dd22326500b623ef590241d01325af1325b
inhwa6854 <inhwa6854@gmail.com>
2024-04-24 22:40:38 +0900

    시험공부중입니다

---
+++ mid_term/js/math.js
@@ -0,0 +1,19 @@
+var a = 10;
+var b = 2;
+
+console.log("덧셈 : " + (a+b));
+console.log("뺄셈 : " + (a-b));
+console.log("곱셈 : " + (a*b));
+console.log("나눗셈 : "+ (a/b));
+console.log("나머지 : " + (a%b));
+console.log("거듭제공 : " + (a**b));
+
+var age =20;
+var result = age>19? "성인입니다" : "미성년자입니다";
+console.log(result);
+
+console.log(true||false);
+
+var age2 = 30;
+var result2 = (age2 >35 || age2<7)? "지원대상입니다" : "지원대상이 아닙니다";
+console.log(result2);
---
+++ mid_term/js/va.js
@@ -0,0 +1,10 @@
+var a = 95;
+var eng = 85;
+var math = 52;
+var total = a + eng + math ;
+var avg = total /3;
+console.log("모두 더한값은 " + total);
+console.log("평균 값은 "+avg);
+
+var msg = "dndsk lfn lsdknf klfsdk dsn lnas ksad";
+console.log("문자열의 길이는 " + msg.length);
